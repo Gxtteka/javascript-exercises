4814f9e738a28b7c2bcdbce11ba6a44fc1a7cc71
Danis <no.0ne.04@mail.ru>
2026-04-06 00:07:53 +0500

    Решил задачу 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    char_for_delete = [',', ' ', '-', '.', '!', '?']
+    text_without_marks = []
+    text_without_marks_reverse = []
+    for (let char of text){
+        if (!char_for_delete.includes(char)){
+            text_without_marks.push(char)
+            text_without_marks_reverse.unshift(char)
+        } 
+    }
+    text_without_marks = text_without_marks.join('')
+    text_without_marks_reverse = text_without_marks_reverse.join('')
 
+    text_without_marks = text_without_marks.toLowerCase()
+    text_without_marks_reverse = text_without_marks_reverse.toLowerCase()
+
+
+    if (text_without_marks == text_without_marks_reverse){
+        return true
+    }else{
+        return false
+    }
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
